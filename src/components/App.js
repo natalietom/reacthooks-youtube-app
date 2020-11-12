b065ca4a18a332e03d3ba654d30cd1154318d9d9
natalietom <ntom33168@yahoo.com>
2020-11-11 21:39:38 -0800

    Initial search term changed

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos("highlights");
+  const [videos, search] = useVideos("workout");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
